068ae0f7702506dbd05a2532be146ba579ac1bd3
KingEmma <etagbor@gmail.com>
2023-05-22 09:46:28 +0000

    added pizza data to ui

--- src/app/components/Pizza.js
+++ src/app/components/Pizza.js
@@ -1,8 +1,16 @@
 'use client'
 import { useState } from "react";
+import Image from "next/image";
+import ReactModal from "react-modal";
+import PizzaDetails from "./PizzaDetails";
+import { IoCloseOutline} from 'react-icons/io5'
 
-const Pizza = () => {
-  return <div>Pizza</div>;
+const Pizza = ({pizza}) => {
+  return <div className="group py-2 bg-blue-200 px-3 xl:py-4 xl:px-2 rounded-r-xl">
+
+  <Image width={270} height={270} src={pizza.image} alt="" priority={1} />
+
+  </div>;
 };
 
 export default Pizza;
