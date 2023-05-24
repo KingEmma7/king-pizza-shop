bc03c82e4113d14e623dc4069e7befb20dbf3511
KingEmma <etagbor@gmail.com>
2023-05-24 09:23:07 +0000

    completed ui for cart in desktop devices

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,7 +1,15 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { CartContext } from "../context/CartContext";
+import { useContext } from "react";
+
 
 const Nav = () => {
+  
+  const {isOpen, setIsOpen, cart} =useContext(CartContext)
+  
   return (
     <nav className="w-full absolute py-3 bg-pink-50/10">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center gap-y-8">
@@ -21,9 +29,9 @@ const Nav = () => {
             </div>
         </div>
         
-        <div className="relative cursor-pointer hidden lg:flex">
+        <div onClick={()=>setIsOpen(!isOpen)} className="relative cursor-pointer hidden lg:flex">
           <Image src={'bag.svg'} alt="" width={38} height={38}/>
-        <div className="bg-tertiary w-6 h-6 rounded-full text-white flex justify-center items-center text-[13px] font-robotoCondensed absolute -bottom-2 -right-1">3</div>
+        <div className="bg-tertiary w-6 h-6 rounded-full text-white flex justify-center items-center text-[13px] font-robotoCondensed absolute -bottom-2 -right-1">{cart.length}</div>
 
         </div>
       </div>
